fdd6a1a597f2a6a0e759a195c8efda9f98c6c8eb
MsldLuxy <310870966@qq.com>
2018-06-21 15:17:16 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -4,12 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+// 解决click点击有300ms延迟的问题
+import fastClick from 'fastclick'
 
+import './assets/styles/reset.css'
+// 解决1像素的问题
+import './assets/styles/border.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
+  router: router,
+  components: { App: App },
   template: '<App/>'
 })
+
+// 路由就是根据网址的不同，返回不同的内容和用户
